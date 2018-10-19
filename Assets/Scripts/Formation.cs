99c4ca5ca1931e4f7e8329c284156aefd4475236
Max Sichel <msik314@gmail.com>
2018-10-19 03:33:03 -0400

    Implement untested scalable formation

---
+++ Assets/Scripts/Formation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Formation : AIBehavior
+{
+    private Pursue pursue;
+    private FormationManager manager;
+
+    void Awake()
+    {
+        manager = Object.FindObjectOfType<FormationManager>();
+    }
+    
+    public override Vector2 get(Vector2 target, Vector2 currentVelocity, Vector2 targetVelocity = new Vector2())
+    {
+        return pursue.get(manager.getTarget(owned.gameObject), currentVelocity, targetVelocity);
+    }
+
+    public override void draw(GameObject target)
+    {
+        pursue.draw(target);
+    }
+
+    public bool isLeader{get; private set;}
+
+    public void setLeader()
+    {
+        isLeader = true;
+    }
+}
---
+++ Assets/Scripts/FormationManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FormationType
+{
+    Scalable,
+    Emergent,
+    TwoLevel
+}
+
+public class FormationManager : MonoBehaviour
+{
+    [SerializeField] private FormationType startingFormation = FormationType.Scalable;
+    [SerializeField] private float radiusScale = 0.16666666f;
+    [SerializeField] private float radiusBias = 0.5f;
+	[SerializeField] private List<GameObject> birds;
+    
+    GameObject leader;
+
+    public static FormationType formation{get; private set;}
+	void Awake()
+    {
+		formation = startingFormation;
+	}
+
+    void Start()
+    {
+        setLeader();
+    }
+
+    public Vector2 getTarget(GameObject obj)
+    {
+        if(formation == FormationType.Scalable)
+        {
+            int index = birds.IndexOf(obj);
+            if(index < 0) return obj.transform.position;
+
+            Vector2 center = leader.transform.position - leader.transform.forward * (radiusScale * birds.Count + radiusBias);
+            return center + (Vector2)(Quaternion.AngleAxis(index * 360.0f/birds.Count, Vector3.zero) * (leader.transform.position - (Vector3)center));
+        }
+
+        return obj.transform.position;
+    }
+
+    public void setLeader()
+    {
+        leader = birds[0];
+        Formation f = leader.GetComponent<Formation>();
+        f.setLeader();
+    }
+}
